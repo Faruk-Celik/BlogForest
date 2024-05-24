05de748198da71498b67f4d93b835beac2568591
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-05-24 12:22:16 +0300

    Add project files.

---
+++ BlogForest.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogForest.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.DataAccessLayer.Context
+{
+    public class BlogContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6VB768D\\SQLEXPRESS02; initial catalog=BlogForestDb2; integrated Security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ BlogForest.DataAccessLayer/EntityFramework/EfBlogDal.cs
@@ -0,0 +1,27 @@
+﻿using BlogForest.DataAccessLayer.Abstract;
+using BlogForest.DataAccessLayer.Context;
+using BlogForest.DataAccessLayer.Repositories;
+using BlogForest.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.DataAccessLayer.EntityFramework
+{
+    public class EfBlogDal : GenericRepository<Blog>, IBlogDal
+    {
+        public EfBlogDal(BlogContext context) : base(context)
+        {
+        }
+
+        public List<Blog> GetBlogsWithCategoryAndUser()
+        {
+            var context = new BlogContext();
+            var values = context.Blogs.Include(x => x.Category).Include(y => y.AppUser).ToList();
+            return values;
+        }
+    }
+}
